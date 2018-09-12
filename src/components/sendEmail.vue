f44cf1457f734cf0b774c2849a9b8e56cf980897
yuanyi <yuanyi@qianbaocard.com>
2018-09-12 17:11:53 +0800

    update createTimedAttention.vue

--- src/components/sendEmail.vue
+++ src/components/sendEmail.vue
@@ -14,7 +14,7 @@
 
 <script>
     export default {
-        props: ['cancleButton', 'confirmButton', 'emailId', 'emailInputChange']
+      props: ['cancleButton', 'confirmButton', 'emailId', 'emailInputChange']
     }
 </script>
 
