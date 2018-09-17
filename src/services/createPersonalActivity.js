59e26c6529a6330cd4b4998f38e61beb08b956ed
weilongfei <weilongfei@qianbaocard.com>
2018-09-17 21:51:07 +0800

    还原createPersonalActivity

--- src/services/createPersonalActivity.js
+++ src/services/createPersonalActivity.js
@@ -1,2 +1,15 @@
 import ServiceBase from './serviceBase'
-export default new ServiceBase('/mc/v1/me/activities')
+import request from './request'
+
+class CreatePersonalActivity extends ServiceBase {
+  createActivity (data) {
+    return request.post(`${this.url}`, data)
+  }
+  getAcitivity (data) {
+    return request.get(`${this.url}?type=${data}`)
+  }
+  putActivity (data) {
+    return request.put(`/mc/v1/activities/${data.id}`, data.request)
+  }
+}
+export default new CreatePersonalActivity('/mc/v1/me/activities')
